11a5144b6cb628880d373e651d0f403c75e33262
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2021-12-17 16:00:41 +0200

    Add some code

--- 02_fex/bot/constants.py
+++ 02_fex/bot/constants.py
@@ -5,6 +5,11 @@ LOCAL_DRIVER = "local"
 # seconds to wait for element to appear
 TIMEOUT = 8
 
-# folder with files
-FILE_FOLDER = "files"
+# folder with files to upload
+UPLOAD_FOLDER = "files_to_upload"
 
+# folder name created in fex
+FEX_FOLDER_NAME = "Folder"
+
+# folder for downloaded files
+DOWNLOAD_FOLDER = "downloaded_files"
--- 02_fex/run.py
+++ 02_fex/run.py
@@ -3,19 +3,28 @@ import sys
 from loguru import logger
 
 from bot.bot import FexBot
-from bot.constants import REMOTE_DRIVER, LOCAL_DRIVER
+from bot.constants import REMOTE_DRIVER, LOCAL_DRIVER, FEX_FOLDER_NAME
 
+files_list = ['file1.txt', 'file2.txt'] #, 'file3.txt']
 
 if __name__ == "__main__":
     r_bot = ""
+    driver_type = sys.argv[1] if len(sys.argv) > 1 else LOCAL_DRIVER
+    logger.debug(f"Driver: {driver_type}")
     try:
-        r_bot = FexBot(driver_type=sys.argv[1] if len(sys.argv) > 1 else LOCAL_DRIVER)
-        r_bot.driver.get("https://fex.net/uk/")
-        r_bot.pass_files(['file1.txt', 'file2.txt', 'file3.txt'])
+        f_bot = FexBot(driver_type=driver_type)
+        f_bot.driver.get("https://fex.net/uk/")
+        f_bot.pass_files(files_list)
+        f_bot.create_folder(FEX_FOLDER_NAME)
+        f_bot.close_black_thing_if_its_there()
+        f_bot.move_files(files_list, FEX_FOLDER_NAME)
+        f_bot.open_folder(FEX_FOLDER_NAME)
+        f_bot.add_to_name(files_list[0], add_to_name="_new")
+        f_bot.remove_file(files_list[1])
 
     except Exception as e:
         logger.error(repr(e))
-        if sys.argv[1] == REMOTE_DRIVER:
+        if driver_type == REMOTE_DRIVER:
             r_bot.driver.quit()
-    if sys.argv[1] == REMOTE_DRIVER:
+    if driver_type == REMOTE_DRIVER:
         r_bot.driver.quit()
