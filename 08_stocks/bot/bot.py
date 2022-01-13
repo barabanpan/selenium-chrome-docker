be7fba89b5457bd0507602cdf603ec76dcfbbcad
Nataliia Dyshko <nataliia.dyshko@jetsoftpro.com>
2022-01-13 14:52:44 +0200

    Finish tracking price changes

--- 08_stocks/bot/bot.py
+++ 08_stocks/bot/bot.py
@@ -15,6 +15,15 @@ from bot.constants import TIMEOUT, CSV_PATH
 from bot.driver.driver import get_edge_driver as get_driver
 
 
+def clean_and_to_float(string):
+    return float(string.strip().replace(',', ''))
+
+
+def get_percent_diff(old_value, new_value):
+    """100(1 - old/new) gives percent diff"""
+    return round(100 * (1 - old_value/new_value), 2)
+
+
 class StocksBot:
     def __init__(self, driver_type):
         self.driver = get_driver(driver_type)
@@ -48,18 +57,31 @@ class StocksBot:
     def write_505_stock_prices_to_csv(self):
         rows = self.find_all('//tbody/tr')[:505]
         data = []
-        for i, row in enumerate(rows):
+        for row in rows:
             tds = row.find_elements_by_xpath('.//td')
             ticker = tds[2].find_element_by_xpath('.//a').text
-            price = float(tds[4].text.strip().replace(',', ''))
+            price = clean_and_to_float(tds[4].text)
             data.append([ticker, price])
         data_df = pd.DataFrame(data=data, columns=["ticker", "price"])
         data_df.to_csv(CSV_PATH, index=False)
         logger.debug("505 rows were written to csv")
 
     def check_price_changes_and_write_to_list(self):
-        for ticker in ["AAPL"]:
-            self.find('//input[@id="yfin-usr-qry"]').send_keys(ticker)
-            self.find('//button[@id="header-desktop-search-button"]').click()
-            value = int(self.find('//span[@class="_11248a25 c916dce9"]').text)
-            # print(f'AAPL = "{value}"')
+        df = pd.read_csv(CSV_PATH)
+        for index, row in df.iterrows():  # generator, not list
+            if index > 10:
+                break
+            ticker = row["ticker"].replace(".", "-")
+            old_price = row["price"]
+            # self.find('//input[@id="yfin-usr-qry"]').send_keys(ticker)
+            # self.find('//button[@id="header-desktop-search-button"]').click()
+            self.driver.get(f"https://finance.yahoo.com/quote/{ticker}?p={ticker}")
+            xpath = '//fin-streamer[@class="Fw(b) Fz(36px) Mb(-4px) D(ib)"]'
+            new_price = clean_and_to_float(self.find(xpath).text)
+            # new_price = clean_and_to_float(self.find('//span[@class="_11248a25 c916dce9"]').text)
+            diff = get_percent_diff(old_price, new_price)
+            if diff >= 1:
+                print(f'TIME TO SELL({diff})! {ticker} - was: "{old_price}", now: "{new_price}"')
+            if diff <= -1:
+                print(f'TIME TO BUY({diff})! {ticker} - was: "{old_price}", now: "{new_price}"')
+        logger.debug("All 505 tickers were processed")
